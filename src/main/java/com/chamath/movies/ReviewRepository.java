e1bd1b660d79d948dad329f4b1ee6974354b3839
chamath <csschamathinduwara@gmail.com>
2023-07-29 01:15:55 +0530

    add reviewing process

---
+++ src/main/java/com/chamath/movies/ReviewRepository.java
@@ -0,0 +1,11 @@
+package com.chamath.movies;
+
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReviewRepository extends MongoRepository<Review, ObjectId> {
+
+
+}
